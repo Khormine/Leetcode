ef7e9fb0b214c5d00de0887db62e3e8b12c457d5
Khormine <romain.roux1@etu.esisar.grenoble-inp.fr>
2023-02-12 21:35:10 +0100

    Modified Sum Two

--- Sum Two/Solution.py
+++ Sum Two/Solution.py
@@ -9,5 +9,8 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-solution = Solution()
-print(solution.twoSum([3,2,4], 6))
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([2,7,11,15], 9))
+    print(solution.twoSum([3,2,4], 6))
+    print(solution.twoSum([3,3], 6))
