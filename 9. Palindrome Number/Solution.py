4c6bd18eef9ed3ff6fcc5015f54fde5bcaad80ee
Khormine <romain.roux1@etu.esisar.grenoble-inp.fr>
2023-02-14 10:43:57 +0100

    Added palindrome number without string convertion

--- 9. Palindrome Number/Solution.py
+++ 9. Palindrome Number/Solution.py
@@ -6,9 +6,24 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         string = str(x)
         return string == string[::-1]
-    
 
-solution = Solution()
-print(solution.isPalindrome(121))
-print(solution.isPalindrome(-121))
-print(solution.isPalindrome(10))
+    # Complexity O(log10 n)    
+    def isPalindrome2(self, x: int) -> bool:
+        if x < 0:
+            return False
+        n = x
+        rev = 0
+        while n:
+            rev = rev * 10 + n % 10
+            n //= 10
+        return x == rev
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isPalindrome(121))
+    print(solution.isPalindrome(-121))
+    print(solution.isPalindrome(10))
+
+    print(solution.isPalindrome2(121))
+    print(solution.isPalindrome2(-121))
+    print(solution.isPalindrome2(10))
